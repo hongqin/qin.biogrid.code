135c829b76c0f1cfbdef3d0d6836a129db39b7d1
Hong Qin <qinstat@gmail.com>
2017-11-20 14:31:58 -0500

    Basic codes to handle biogrid yeast interaction data

---
+++ 1.clean.tab2.20171115.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl
+
+# usage:
+# ~/pypeworks/biogrid/00_cleanup_biogrid_table.pl  BIOGRID-ORGANISM-Homo_sapiens-3.2.117.tab2.txt > BIOGRID-ORGANISM-Homo_sapiens-3.2.117.tab2.clean.txt
+# can use bash to apply it to more species as needed
+
+while ( <>) {
+    chomp;
+    @aux = split "\t";
+    @new = ();
+    foreach (@aux) {
+        $blah = $_;
+        $blah =~ s/\s//g;
+        ($blah eq '-') && ($blah='');
+        if (! $blah) {
+            push @new, 'NA';
+        } else {
+            # somebody in BioGRID thought it would be cute to use backslash
+            # as quotes
+            $blah = $_;
+            $blah =~ s/\\//g;
+            push @new, $blah;
+        }
+    }
+    
+    print join ("\t", @new), "\n";
+}
