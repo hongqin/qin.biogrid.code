10bec656fc34173245481b696b6a0d6e387f6e74
Hong Qin <qinstat@gmail.com>
2017-11-28 03:09:00 -0500

    add citation to perl code

--- 1.clean.tab2.20171115.pl
+++ 1.clean.tab2.20171115.pl
@@ -3,6 +3,7 @@
 # usage:
 # ~/pypeworks/biogrid/00_cleanup_biogrid_table.pl  BIOGRID-ORGANISM-Homo_sapiens-3.2.117.tab2.txt > BIOGRID-ORGANISM-Homo_sapiens-3.2.117.tab2.clean.txt
 # can use bash to apply it to more species as needed
+# modified from https://github.com/ivanamihalek/biogrid/blob/master/00_cleanup_biogrid_table.pl
 
 while ( <>) {
     chomp;
